fab25a29083dafb9882ed5c727f866be0a63cb30
pythoncoder1234 <fdsnail2@gmail.com>
2024-01-20 12:22:43 -0800

    small changes

--- shakespeare/shakespeare.py
+++ shakespeare/shakespeare.py
@@ -1,27 +1,49 @@
-import re
-
 import numpy as np
 import torch
-from sklearn.preprocessing import OneHotEncoder
-from torch.nn import LSTM, CrossEntropyLoss
+import torch.nn as nn
+from torch.nn import CrossEntropyLoss
+from torch.optim import Adam
 from torch.utils.data import Dataset, DataLoader
 
-seq_length = 20
-import torch
-import torch.nn as nn
+def parse_text():
+    with open("alllines.txt") as f:
+        lines = f.readlines()
+
+    lines = list(map(lambda line: line.replace('"', ''), lines))
+    everything = "".join(lines).lower()
+    everything = "".join(filter(lambda x: x in characters, everything))
+    print("Text parsed")
+    return everything
+
+def preprocess_text():
+    with open("everything.txt") as f:
+        everything = f.read()
+
+    encoding_dict: dict[str, int] = {character: i for i, character in enumerate(characters)}
+    x = np.zeros((len(everything), seq_length, len(characters)), dtype=bool)
+    y = np.zeros((len(everything) - 1, len(characters)), dtype=bool)
+
+    for i in range(len(everything) - seq_length - 1):
+        for j in range(seq_length):
+            character = everything[i + j]
+            x[i, j, encoding_dict[character]] = 1
+        y[i, encoding_dict[everything[i + seq_length + 1]]] = 1
+
+    return x, y
 
 class SimpleLSTM(nn.Module):
     def __init__(self, input_size, hidden_size, output_size, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        super(SimpleLSTM, self).__init()
 
+        # input_size = hidden_size = seq_length = 40
         self.hidden_size = hidden_size
-
-        self.lstm = nn.LSTM(input_size, hidden_size)
+        self.lstm = nn.LSTM(input_size, hidden_size, 2, batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
 
-    def forward(self, input, hidden):
-        lstm_out, hidden = self.lstm(input.view(1, 1, -1), hidden)
+    def forward(self, inputs, hidden):
+        global inputs1
+        inputs1 = inputs
+        lstm_out, hidden = self.lstm(inputs, hidden)
         output = self.fc(lstm_out.view(1, -1))
         return output, hidden
 
@@ -29,24 +51,28 @@ class SimpleLSTM(nn.Module):
         return (torch.zeros(1, 1, self.hidden_size),
                 torch.zeros(1, 1, self.hidden_size))
 
+
 class CustomDataset(Dataset):
     def __init__(self, features, labels):
-        self.features = torch.FloatTensor(features)
-        self.labels = torch.FloatTensor(labels)
+        self.features = features
+        self.labels = labels
 
     def __len__(self):
         return len(self.features)
 
     def __getitem__(self, idx):
-        return self.features[idx], self.labels[idx]
+        return self.features[idx].astype(np.float32), self.labels[idx].astype(np.float32)
+
 
 def train(model, train_loader, criterion, optimizer, num_epochs):
+    global inputs
     train_loss = []
 
     for epoch in range(num_epochs):
         total_loss = 0.0
 
         for i, (inputs, labels) in enumerate(train_loader):
+            inputs = torch.FloatTensor(inputs)
             optimizer.zero_grad()
             outputs, _ = model(inputs, None)
             loss = criterion(outputs, labels)
@@ -61,27 +87,16 @@ def train(model, train_loader, criterion, optimizer, num_epochs):
 
     return train_loss
 
-with open("alllines.txt") as f:
-    lines = f.readlines()
+special_chars = list("!?.,'\n ")
+characters = np.array([chr(i) for i in range(97, 97 + 26)] + special_chars)
 
-lines = list(map(lambda line: line[1:-2], lines))
-everything = " ".join(lines).replace("--", " ").replace("\t", "")
-everything = re.sub('\S+', lambda m: re.sub('^\W+|\W+$', '', m.group()), everything).lower()
-text = everything.split(" ")
-words = np.unique(text)
-print("Text parsed")
+# x, y = preprocess_text()
+x, y = np.load("inputs.npy"), np.load("outputs.npy")
 
-vector = OneHotEncoder().fit_transform(words.reshape(-1, 1)).toarray()
-encoding_dict = {word: encoding for word, encoding in zip(words, vector)}
-x = []
-y = []
+seq_length = 40
 
-for i in range(len(text) - seq_length - 1):
-    x.append(list(map(lambda word: encoding_dict[word], text[i:i + seq_length])))
-    y.append(encoding_dict[text[i+seq_length]])
-
+print("Preprocessing completed")
 print("Data loader")
 data_loader = DataLoader(CustomDataset(x, y), batch_size=10)
-print("Preprocessing completed")
 model = SimpleLSTM(seq_length, seq_length, 1)
-train(model, data_loader, CrossEntropyLoss(), torch.Adam(), 5)
+train(model, data_loader, CrossEntropyLoss(), Adam(model.parameters(), lr=0.01), 5)
